34d7a5d9dcc1c152cadee79e38ef734cd3b09cb0
Rafael Castilho Carvalho <rafacien.rc@gmail.com>
2026-06-01 09:50:31 -0400

    Change env

--- back/src/index.ts
+++ back/src/index.ts
@@ -37,6 +37,7 @@ app.use((req, res, next) => {
   if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
