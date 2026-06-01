51d68f93e03ff33ff5d554b04417a3d00cfb7764
Rafael Castilho Carvalho <rafacien.rc@gmail.com>
2026-05-31 23:42:33 -0400

    Update CORS

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://laseropsback.rcchome.com.br/v1',
+  baseURL: 'https://laseropsback.rcchome.com.br/v1',
   withCredentials: true,
 });
 
